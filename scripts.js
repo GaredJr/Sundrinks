32d4643398948e094e7e2572a0dcd1100330a309
GaredJr <125034872+GaredJr@users.noreply.github.com>
2023-05-12 18:38:40 +0200

    asds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 function AddToCart(ele) {
+	if (ele.innerHTML = "Add To Cart") {
+		document.getElementById("cartnum").innerHTML = parseInt(document.getElementById("cartnum").innerHTML) + 1;
+	}
 	ele.innerHTML = "Added"
 }
